85dc7255771130c86be9a6a412bebf728d594092
SeojinSeojin <1106laura@naver.com>
2021-04-04 03:10:05 +0900

    [Feat] ConstraintLayout, ViewBinding

---
+++ app/src/main/java/org/sopt/soptseminar_week1/NewActivity.kt
@@ -0,0 +1,11 @@
+package org.sopt.soptseminar_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+    }
+}
