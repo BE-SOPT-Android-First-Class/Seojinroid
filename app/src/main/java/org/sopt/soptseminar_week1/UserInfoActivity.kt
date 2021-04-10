c256a6d2313ab165eecfec2bc01e78ba7876521b
SeojinSeojin <1106laura@naver.com>
2021-04-10 14:37:43 +0900

    [Feat] UserInfoActivity 생성

---
+++ app/src/main/java/org/sopt/soptseminar_week1/UserInfoActivity.kt
@@ -0,0 +1,14 @@
+package org.sopt.soptseminar_week1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.soptseminar_week1.databinding.ActivityUserInfoBinding
+
+class UserInfoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUserInfoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUserInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
